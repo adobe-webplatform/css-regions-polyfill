b05ee167d9a53566a60bb78528e49cb94804fcee
Mihai Corlan <mcorlan@gmail.com>
2012-09-10 20:35:47 +0300

    adding files

---
+++ playground/content-folding/test.js
@@ -0,0 +1,28 @@
+// Generic Testing Function
+var supports = (function() {  
+   var div     = document.createElement('div'),  
+       vendors = 'Khtml Ms O Moz Webkit'.split(' '),  
+       len     = vendors.length;  
+  
+   return function(prop) {  
+      if ( prop in div.style ) return true;  
+  
+      prop = prop.replace(/^[a-z]/, function(val) {  
+         return val.toUpperCase();  
+      });  
+  
+      while(len--) {  
+         if ( vendors[len] + prop in div.style ) {   
+            return true;  
+         }  
+      }  
+      return false;  
+   };  
+})();  
+  
+// Test
+if ( supports('flowInto') ) {
+   document.documentElement.className += ' cssregions';
+} else {
+    document.documentElement.className += ' no-cssregions';
+}
