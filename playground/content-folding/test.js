5457ea655d4c9c6edbb0cf5b7b38b6555ebc248d
Mihai Corlan <mcorlan@gmail.com>
2012-09-11 17:01:11 +0300

    Start parsing the CSS

--- playground/content-folding/test.js
+++ playground/content-folding/test.js
@@ -1,28 +1,29 @@
 // Generic Testing Function
-var supports = (function() {  
-   var div     = document.createElement('div'),  
-       vendors = 'Khtml Ms O Moz Webkit'.split(' '),  
-       len     = vendors.length;  
-  
-   return function(prop) {  
-      if ( prop in div.style ) return true;  
-  
-      prop = prop.replace(/^[a-z]/, function(val) {  
-         return val.toUpperCase();  
-      });  
-  
-      while(len--) {  
-         if ( vendors[len] + prop in div.style ) {   
-            return true;  
-         }  
-      }  
-      return false;  
-   };  
-})();  
-  
+//var supports = (function() {
+//   var div     = document.createElement('div'),
+//       vendors = 'Khtml Ms O Moz Webkit'.split(' '),
+//       len     = vendors.length;
+//
+//   return function(prop) {
+//      if ( prop in div.style ) return true;
+//
+//      prop = prop.replace(/^[a-z]/, function(val) {
+//         return val.toUpperCase();
+//      });
+//
+//      while(len--) {
+//         if ( vendors[len] + prop in div.style ) {
+//            return true;
+//         }
+//      }
+//      return false;
+//   };
+//})();
+
+document.documentElement.className += ' cssregions';
 // Test
-if ( supports('flowInto') ) {
-   document.documentElement.className += ' cssregions';
-} else {
-    document.documentElement.className += ' no-cssregions';
-}
+//if (Modernizr.regions) {
+//   document.documentElement.className += ' cssregions';
+//} else {
+//    document.documentElement.className += ' no-cssregions';
+//}
