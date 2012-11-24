9f51a8202445e05cb9b251f381378c889a3f2a3b
Razvan Caliman <razvan.caliman@gmail.com>
2012-11-24 01:22:33 +0000

    basic gruntfile for building polyfill

---
+++ grunt.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+    // Project configuration.
+    grunt.initConfig({
+      pkg: '<json:package.json>',
+      
+      concat: {
+        dist: {
+          src: ['playground/src/cssregions.js'],
+          dest: '<%= pkg.name %>.js'
+        }
+      },
+      
+      min: {
+          dist: {
+              src: ['<%= concat.dist.dest %>'],
+              dest: '<%= pkg.name %>.min.js'
+          }
+      },
+      
+      watch: {
+          js: {
+              files: ['playground/src/cssregions.js'],
+              tasks: 'concat'
+          }
+      }
+    });
+    
+    grunt.registerTask('default', 'concat min');
+    
+};
